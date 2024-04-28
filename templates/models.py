e0f221f086af52263eba3894dc88880f7c71c0c7
RedSSD <salatcaesarrr1@gmail.com>
2024-04-28 18:29:32 +0200

    changed models

--- templates/models.py
+++ templates/models.py
@@ -3,13 +3,13 @@ from django.contrib.postgres.fields import ArrayField
 
 
 class UserGroup(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, blank=False, null=False)
     owner = models.ForeignKey(
         to='authentication.CustomUser',
         related_name='user_groups',
         on_delete=models.PROTECT,
-        blank=True,
-        null=True
+        blank=False,
+        null=False
     )
     receivers = ArrayField(ArrayField(models.CharField(max_length=255)), blank=True)
 
@@ -18,8 +18,8 @@ class UserGroup(models.Model):
 
 
 class NotificationTemplate(models.Model):
-    name = models.CharField(max_length=40)
-    text = models.TextField(max_length=120)
+    name = models.CharField(max_length=40, blank=False, null=False)
+    text = models.TextField(max_length=120, blank=False, null=False)
     group = models.ForeignKey(
         to='templates.UserGroup',
         on_delete=models.PROTECT,
